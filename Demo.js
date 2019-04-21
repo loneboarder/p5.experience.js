da297b91e7f50bdea51e2c88fc9cf4dbe0d99d71
loneboarder <49822131+loneboarder@users.noreply.github.com>
2019-04-21 23:40:45 +0200

    Update Demo.js

--- Demo.js
+++ Demo.js
@@ -11,12 +11,6 @@ function createApp() {
 
   a = uxElement('rect', 50, 50, 50, 30);
   b = uxElement('ellipse', 300, 300, 100, 150);
-
-
-  uxStroke(200,0,0);
-  uxNoFill();
-  uxFill(255);
-  uxNoStroke();
   c = uxElement('triangle', 100, 100, 150, 100, 125, 150);
 
   a.inp('hover', triggered);
@@ -33,5 +27,5 @@ function draw() {
 }
 
 function triggered() {
-  b.uxFill = [100, 200, 300];
+  a.uxFill = [100, 200, 300];
 }
